695c0ab421717e9d21f2ea29de225c035b8f4f5b
AbdUlrahman <78540707+AbdUlrahman167@users.noreply.github.com>
2021-10-23 22:05:47 +0200

    Update buyLotsOfFruit.py

--- buyLotsOfFruit.py
+++ buyLotsOfFruit.py
@@ -36,6 +36,15 @@ def buyLotsOfFruit(orderList):
     """
     totalCost = 0.0
     "*** YOUR CODE HERE ***"
+    for fruit, cost in orderList:
+        if fruit not in fruitPrices:
+            print("Sorry %s it doesn't exist" % (fruit))
+            print("Edit your list,please")
+            return None
+        else:
+            totalCost += cost * fruitPrices[fruit]
+
+    
     return totalCost
 
 
@@ -44,3 +53,36 @@ if __name__ == '__main__':
     "This code runs when you invoke the script from the command line"
     orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
     print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
+    
+    
+    
+#if you wanna use input orders from user follow this code
+'''
+# create your orderList
+n = int(input("No.kinds: "))
+d = {}
+for i in range(n):
+    key = str(input("fruit: "))
+    value = float(input("No.= "))
+    d[key] = value
+orderlist = list(d.items())
+print("Your order is %s and its cost's" % (orderlist))
+
+# following line depends on owner of shop
+price_of_fruits = {'Apples': 20.00, 'Mango': 25.00, 'Oranges': 15.00,
+                   'Banana': 15.00, 'Strawberries': 15.00, 'Clementine': 5.00}
+
+# cal.its cost
+def buy_lots_of_fruit(orderlist):
+    cost = 0.0
+    for key, value in orderlist:
+        if key not in price_of_fruits:
+            print("Sorry %s it doesn't exist" % (key))
+            print("Edit your list,please")
+            return None
+        else:
+            cost += value * price_of_fruits[key]
+
+    return cost
+print(buy_lots_of_fruit(orderlist))
+'''
